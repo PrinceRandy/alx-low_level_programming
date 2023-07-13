ecc5dd1b691f6dc14e61308cd479236c16e7dbe3
Prince Abbey <princeabbeydduke@gmail.com>
2023-07-13 16:43:34 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * *  main	- Program that prints a line with puts function
- *   * Return: Always 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
-i}
+}
