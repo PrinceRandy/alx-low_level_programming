532e628971dfd97cfc2eacd3a58412f61d3ceca1
Prince Abbey <princeabbeydduke@gmail.com>
2023-07-21 17:16:40 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - makes a straight line
+ * @n: number of times the line is to be printed
+ * Description: Can only use _putchar to print
+ * Return: None.
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 2; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
