e375b8366fcc8cd726cab87ec18282c3c81cabd4
Prince Abbey <princeabbeydduke@gmail.com>
2023-07-27 19:19:56 -0700

    0x06- pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
-	char *p;
+ *  * rot13 - translates string to ROT13
+ *   * @s: input string to be translated
+ *    *
+ *     * Return: string after conversion of ROT13
+ *      */
 
-	p = rot13(s);
-	printf("%s", p);
-	printf("%s", p);
-	printf("------------------------------------\n");
-	printf("%s", s);
-	printf("------------------------------------\n");
-	p = rot13(s);
-	printf("%s", p);
-	printf("------------------------------------\n");
-	printf("%s", s);
-	printf("------------------------------------\n");
-	p = rot13(s);
-	printf("%s", p);
-	printf("------------------------------------\n");
-	printf("%s", s);
-	return (0);
+char *rot13(char *s)
+{
+		int i, j;
+			char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				char rot_it[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+					for (i = 0; s[i] != '\0'; i++)
+								for (j = 0; j < 52; j++)
+												if (s[i] == alphabet[j])
+																	s[i] = rot_it[j], j = 52;
+						return (s);
 }
