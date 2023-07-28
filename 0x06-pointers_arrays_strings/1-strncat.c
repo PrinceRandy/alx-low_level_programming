2d47295b570ee71217a1bba5324112fd66d6a877
Prince Abbey <princeabbeydduke@gmail.com>
2023-07-27 18:44:22 -0700

    0x06- pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strncat - concatenates string with n bytes from another string
+ * _strncat - concatenates string with n bytes from another string
  * @dest: destination string
  * @src: source string
  * @n: bytes to be copied
