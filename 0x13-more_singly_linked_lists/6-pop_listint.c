4bb02bce7847dff1b9b89a5925a7c197004f5646
Prince Abbey <princeabbeydduke@gmail.com>
2023-08-29 17:28:36 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node
+ * @head: pointer to the head of the linked list
+ *
+ * Return: head node's data (n)
+ * or zero; if list is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	temp = *head;
+
+	if (temp == NULL)
+	return (0);
+
+	*head = temp->next;
+	n = temp->n;
+	free(temp);
+	return (n);
+}
